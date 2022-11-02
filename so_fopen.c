593e5e8ccb8f8302f4f451f9d7e031a1669261cc
war1u5 <marius.wsf@gmail.com>
2022-11-02 23:45:21 +0200

    made it shorter and easier to read

--- so_fopen.c
+++ so_fopen.c
@@ -67,171 +67,76 @@ struct _so_file {
 };
 
 SO_FILE* so_fopen(const char* pathname, const char* mode) {
-
+	int flags = -1;//O_RDONLY;
+	int plus = 0;
+	int fd;
 	SO_FILE* file = NULL;
-//------------------------------r---------------------------------------
-	if (strcmp(mode, "r") == 0) {
-		SO_FILE* file = (SO_FILE*)malloc(sizeof(SO_FILE));
-		if (file == NULL) {
-			printf("eroare malloc\n");
+
+	while(*mode){
+		switch (*mode)
+		{
+		case 'r':
+			file = (SO_FILE*)malloc(sizeof(SO_FILE)); 
+			flags = O_RDONLY;
+			file->_canRead = SO_TRUE;
+			file->_canWrite = SO_FALSE;
+			file->_append = SO_FALSE;
+			file->_update = SO_FALSE;
+			break;
+		case 'w':
+			file = (SO_FILE*)malloc(sizeof(SO_FILE)); 
+			flags = O_WRONLY | O_TRUNC | O_CREAT;
+			file->_canRead = SO_FALSE;
+			file->_canWrite = SO_TRUE;
+			file->_append = SO_FALSE;
+			file->_update = SO_FALSE;
+			break;
+		case 'a':
+			file = (SO_FILE*)malloc(sizeof(SO_FILE)); 
+			flags = O_APPEND | O_WRONLY | O_CREAT;
+			file->_canRead = SO_FALSE;
+			file->_canWrite = SO_TRUE;
+			file->_append = SO_TRUE;
+			file->_update = SO_FALSE;
+			break;
+		case 'r+':
+			file = (SO_FILE*)malloc(sizeof(SO_FILE)); 
+			flags = O_RDWR;
+			file->_canRead = SO_TRUE;
+			file->_canWrite = SO_TRUE;
+			file->_append = SO_FALSE;
+			file->_update = SO_TRUE;
+			break;
+		case 'w+':
+			file = (SO_FILE*)malloc(sizeof(SO_FILE)); 
+			flags = O_RDWR | O_TRUNC | O_CREAT;
+			file->_canRead = SO_TRUE;
+			file->_canWrite = SO_TRUE;
+			file->_append = SO_FALSE;
+			file->_update = SO_TRUE;
+			break;
+		case 'a+':
+			file = (SO_FILE*)malloc(sizeof(SO_FILE)); 
+			flags = O_APPEND | O_RDWR | O_CREAT;
+			file->_canRead = SO_TRUE;
+			file->_canWrite = SO_TRUE;
+			file->_append = SO_TRUE;
+			file->_update = SO_TRUE;
+		break; 
+		default:
+			printf("no such mode\n");
 			return NULL;
+			break;
 		}
-	#if defined(__linux__)
-		file->_fHandle = open(pathname, O_RDONLY);
-		if(file->_fHandle == -1) {
-			printf("open\n");
-			free(file);
-			return NULL;
-		}
-
-	#elif defined(_WIN32)
-	//todo
-
-	#endif
-		file->_canRead = SO_TRUE;
-		file->_canWrite = SO_FALSE;
-		file->_append = SO_FALSE;
-		file->_update = SO_FALSE;
-
-		return file;
+		mode++;
 	}
 
-//------------------------------w---------------------------------------
-	else if (strcmp(mode, "w") == 0) {
-		SO_FILE* file = (SO_FILE*)malloc(sizeof(SO_FILE));
-		if (file == NULL) {
-			printf("eroare malloc\n");
-			return NULL;
-		}
 	#if defined(__linux__)
-		file->_fHandle = open(pathname, O_WRONLY | O_TRUNC | O_CREAT, 0664);
-		if(file->_fHandle == -1) {
-			printf("open\n");
-			free(file);
-			return NULL;
-		}
+		file->_fHandle = open(pathname, flags, 0644);
+	
 	#elif defined(_WIN32)
 	//todo
-
 	#endif
-		file->_canRead = SO_FALSE;
-		file->_canWrite = SO_TRUE;
-		file->_append = SO_FALSE;
-		file->_update = SO_FALSE;
-
-		return file;
-	}
-
-
-
-//------------------------------a---------------------------------------
-	else if (strcmp(mode, "a") == 0) {
-		SO_FILE* file = (SO_FILE*)malloc(sizeof(SO_FILE));
-		if (file == NULL) {
-			printf("eroare malloc\n");
-			return NULL;
-		}
-	#if defined(__linux__)
-		file->_fHandle = open(pathname, O_APPEND | O_WRONLY | O_CREAT, 0644);
-		if(file->_fHandle == -1) {
-			printf("open\n");
-			free(file);
-			return NULL;
-		}
-	#elif defined(_WIN32)
-	//todo
-
-	#endif
-		file->_canRead = SO_FALSE;
-		file->_canWrite = SO_TRUE;
-		file->_append = SO_TRUE;
-		file->_update = SO_FALSE;
-
-		return file;	
-	}
-
-
-
-//------------------------------r+---------------------------------------
-	else if (strcmp(mode, "r+") == 0) {
-		SO_FILE* file = (SO_FILE*)malloc(sizeof(SO_FILE));
-		if (file == NULL) {
-			printf("eroare malloc\n");
-			return NULL;
-		}
-	#if defined(__linux__)
-		file->_fHandle = open(pathname, O_RDWR);
-		if(file->_fHandle == -1) {
-			printf("open\n");
-			free(file);
-			return NULL;
-		}
-	#elif defined(_WIN32)
-	//todo
-
-	#endif
-		file->_canRead = SO_TRUE;
-		file->_canWrite = SO_TRUE;
-		file->_append = SO_FALSE;
-		file->_update = SO_TRUE;	
-
-		return file;
-	}
-
-
-
-//------------------------------w+---------------------------------------
-	else if (strcmp(mode, "w+") == 0) {
-		SO_FILE* file = (SO_FILE*)malloc(sizeof(SO_FILE));
-		if (file == NULL) {
-			printf("eroare malloc\n");
-			return NULL;
-		}
-	#if defined(__linux__)
-		file->_fHandle = open(pathname, O_RDWR | O_TRUNC | O_CREAT, 0644);
-		if(file->_fHandle == -1) {
-			printf("open\n");
-			free(file);
-			return NULL;
-		}
-	#elif defined(_WIN32)
-	//todo
-
-	#endif
-		file->_canRead = SO_TRUE;
-		file->_canWrite = SO_TRUE;
-		file->_append = SO_FALSE;
-		file->_update = SO_TRUE;
-
-		return file;		
-	}
-
-
-//------------------------------a+---------------------------------------
-	else if (strcmp(mode, "a+") == 0) {
-		SO_FILE* file = (SO_FILE*)malloc(sizeof(SO_FILE));
-		if (file == NULL) {
-			printf("eroare malloc\n");
-			return NULL;
-		}
-	#if defined(__linux__)
-		file->_fHandle = open(pathname, O_APPEND | O_RDWR | O_CREAT, 0644);
-		if(file->_fHandle == -1) {
-			printf("open\n");
-			free(file);
-			return NULL;
-		}
-	#elif defined(_WIN32)
-	//todo
-
-	#endif
-		file->_canRead = SO_TRUE;
-		file->_canWrite = SO_TRUE;
-		file->_append = SO_TRUE;
-		file->_update = SO_TRUE;	
-
-		return file;		
-	}
 
 	if (file != NULL) {
 		file->_buffer_base = (char*)malloc(sizeof(char) * SO_BUFFER_SIZE);
